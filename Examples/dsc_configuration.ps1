b2fc6d4bd4b2eb4bec1667ba6a367a1bddb368b7
Joe Beaudry <joe.beaudry@slalom.com>
2017-01-11 11:36:18 -0500

    first commit

---
+++ Examples/dsc_configuration.ps1
@@ -0,0 +1,27 @@
+﻿Configuration Sample_xWebsite
+{
+
+    # Import the module that defines custom resources
+    Import-DscResource -Module xWebAdministration
+    Import-DscResource –Module PSDesiredStateConfiguration
+    
+    Node $env:ComputerName
+    {
+        # Install the IIS role
+        WindowsFeature IIS
+        {
+            Ensure          = "Present"
+            Name            = "Web-Server"
+        }
+        # Stop the default website
+        xWebsite DefaultSite
+        {
+            Ensure          = "Present"
+            Name            = "Default Web Site"
+            PhysicalPath    = "C:\Websites\index.html"
+            State           = "Started"
+            #PhysicalPath    = "C:\inetpub\wwwroot"
+            DependsOn       = "[WindowsFeature]IIS"
+        }
+    }
+}
